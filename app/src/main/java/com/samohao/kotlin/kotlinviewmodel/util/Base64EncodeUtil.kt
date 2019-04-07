9196fcca583e030167f10756cdb336803a83efcd
DESKTOP-12N4CAJ\samoh <somanghao@gmail.com>
2019-04-08 08:13:07 +0900

    base64

--- app/src/main/java/com/samohao/kotlin/kotlinviewmodel/util/Base64EncodeUtil.kt
+++ app/src/main/java/com/samohao/kotlin/kotlinviewmodel/util/Base64EncodeUtil.kt
@@ -1,23 +1,14 @@
 package com.samohao.kotlin.kotlinviewmodel.util
 
-import android.content.Context
 import android.util.Base64
-import java.io.File
 import java.lang.Exception
-
-//    private const val TAG = "KOTLIN_TEST"
-//    private const val KEY_AUTH = "KEY_AUTH"
-//
-//    fun setAuthCookie(context : Context , cookie : Set<String>) {
-//        context.getSharedPreferences("KOTLIN_TEST" , Context.MODE_PRIVATE).edit().putStringSet(KEY_AUTH ,cookie)
-//    }
 
 
     class Base64EncodeUtil {
         companion object {
 
             fun encoder(input: String): String{
-                var byteInput : ByteArray
+                val byteInput : ByteArray
                 var output : String
                 try {
                     byteInput = input.toByteArray(Charsets.UTF_8)
